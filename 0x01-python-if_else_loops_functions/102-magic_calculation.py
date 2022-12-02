344dd609600ca608daa15a95be6292816f44680f
abiolaede <abiola2m4ac@gmail.com>
2022-12-01 18:03:18 -0800

    add readme

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python3
-def magic_calculation(a, b):
-    return (98 + a ** b)
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    return a * b - c
